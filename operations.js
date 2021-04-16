6e4437023d610640d55feb0b0911a01424740133
Danielle Robitshek <drobi710@gmail.com>
2021-04-15 21:52:48 -0400

    Node Callback Hell and Promises formatted code

--- operations.js
+++ operations.js
@@ -1,19 +1,19 @@
 exports.insertDocument = (db, document, collection) => {
-    const coll = db.collection(collection);
-    return coll.insertOne(document);
+  const coll = db.collection(collection);
+  return coll.insertOne(document);
 };
 
 exports.findDocuments = (db, collection) => {
-    const coll = db.collection(collection);
-    return coll.find({}).toArray();
+  const coll = db.collection(collection);
+  return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection) => {
-    const coll = db.collection(collection);
-    return coll.deleteOne(document);
+  const coll = db.collection(collection);
+  return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection) => {
-    const coll = db.collection(collection);
-    return coll.updateOne(document, { $set: update }, null);
+  const coll = db.collection(collection);
+  return coll.updateOne(document, { $set: update }, null);
 };
